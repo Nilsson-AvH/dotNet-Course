8471f059bb0320c01d99020f3f949fbb007f3190
NilssonLoRi <nlopez153@misena.edu.co>
2023-11-07 16:37:13 -0500

    Video 042

---
+++ 042 HerenciaII_Sintaxis/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HerenciaII_Sintaxis
+{
+    class Mamiferos
+    {
+        // Constructor Objects
+
+        // Attrib class fields
+
+        // Getter and Setter methods
+
+        // Methods
+        public void respirar()
+        {
+            Console.WriteLine("Soy capaz de respirar.");
+        }
+
+        public void cuidarCrias()
+        {
+            Console.WriteLine("Puedo cuidar de mis crias hasta que se valgan por si solas.");
+        }
+    }
+    class Caballo:Mamiferos
+    {
+        // Constructor objects
+
+        // Attrib class fields
+
+        // Getter and Setter methods
+
+        // Methods
+        public void galopar()
+        {
+            Console.WriteLine("Soy capaz de galopar.");
+        }
+    }
+    class Humano:Mamiferos
+    {
+        // Constructor objects
+
+        // Atribs class fields
+
+        // Getter ads Setter methods
+
+        // Methods
+        public void pensar()
+        {
+            Console.WriteLine("Soy capaz de pensar.");
+        }
+    }
+    class Gorila:Mamiferos
+    {
+        // Constructor objects
+
+        // Attrib class fields
+
+        // Getter and Setter methods
+
+        // Methods
+        public void trepar()
+        {
+            Console.WriteLine("Soy capaz de trepar.");
+        }
+    }
+    class Program
+    {
+        // Constructor objects
+
+        // Attrib class field
+
+        // Getter and Setter methods
+
+        // Methods
+
+        // Main Method
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello, World!");
+            Caballo Babieca = new Caballo();
+
+            Humano Juan = new Humano();
+
+            Gorila Copito = new Gorila();
+
+            Copito.trepar();
+            Copito.respirar();
+        }
+    }
+}
+
